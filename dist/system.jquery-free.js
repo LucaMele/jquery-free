1fd77a65cd845bbf6314bf60ee8cd032553c1e2c
Luca Mele <luca.mele@atos.net>
2016-09-29 13:03:38 +0200

    fisrt working one

---
+++ dist/system.jquery-free.js
@@ -0,0 +1,20 @@
+System.register("jquery-free", [], function (_export) {
+    "use strict";
+
+    var Example;
+
+    function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+    return {
+        setters: [],
+        execute: function () {
+            Example = function Example() {
+                _classCallCheck(this, Example);
+
+                this.isExampleClass = true;
+            };
+
+            _export("default", Example);
+        }
+    };
+});
